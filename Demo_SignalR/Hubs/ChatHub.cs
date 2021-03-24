302a5b306a83c8fe2f20d87ac211ff1e3be420da
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-03-24 18:23:45 +0100

    Ajoutez des fichiers projet.

---
+++ Demo_SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task EnvoyerMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("RecevoirMessage", username, message);
+        }
+    }
+}
